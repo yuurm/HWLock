d58410ad041b84695840e63b5c2b96bf258e6056
yuurm <73988712+yuurm@users.noreply.github.com>
2023-06-11 18:44:20 +0300

    add HW Tasks

---
+++ HWTasks/ProductCard.cs
@@ -0,0 +1,7 @@
+namespace HWTasks;
+
+public class ProductCard
+{
+    public int ProductId { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ HWTasks/Program.cs
@@ -0,0 +1,41 @@
+﻿using HWTasks;
+
+        ProductCard[] cards = new ProductCard[]
+        {
+            new ProductCard { ProductId = 1, Price = 10.0m },
+            new ProductCard { ProductId = 2, Price = 20.0m },
+            new ProductCard { ProductId = 3, Price = 30.0m },
+        };
+
+       
+        decimal totalSum = await CalculateTotalSumParallel(cards);
+
+        Console.WriteLine($"Общая сумма продуктов: {totalSum}");
+    
+
+    static async Task<decimal> CalculateTotalSumParallel(ProductCard[] cards)
+    {
+        decimal totalSum = 0;
+        Task<decimal>[] tasks = new Task<decimal>[cards.Length];
+
+        for (int i = 0; i < cards.Length; i++)
+        {
+            int index = i;
+            tasks[i] = Task.Run(() => CalculateCardSum(cards[index]));
+        }
+
+        await Task.WhenAll(tasks);
+
+        foreach (var task in tasks)
+        {
+            totalSum += task.Result;
+        }
+
+        return totalSum;
+    }
+
+    static decimal CalculateCardSum(ProductCard card)
+    {
+        
+        return card.Price;
+    }
