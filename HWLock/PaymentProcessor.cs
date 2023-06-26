3f76347cafdde7bd8d34eb0e7602ab16755f40db
yuurm <73988712+yuurm@users.noreply.github.com>
2023-06-26 13:41:13 +0300

    add Task cancelling HW

--- HWLock/PaymentProcessor.cs
+++ HWLock/PaymentProcessor.cs
@@ -1,37 +1,71 @@
 namespace HWLock;
 
 public class PaymentProcessor
-{
-    private List<decimal> History { get; set; }
-    private object historyLock;
-    
-    public PaymentProcessor()
     {
-        History = new List<decimal>();
-        historyLock = new object();
-    }
+        private List<decimal> History { get; set; }
+        private object historyLock;
+        private decimal MaxBalance { get; }
+        private decimal MinBalance { get; }
 
-    public void AddPayment(decimal amount)
-    {
-        lock (historyLock) 
+        public PaymentProcessor(decimal maxBalance, decimal minBalance)
         {
-            
-            History.Add(amount);
-            Console.WriteLine($"Платеж на сумму {amount} добавлен.");
+            History = new List<decimal>();
+            historyLock = new object();
+            MaxBalance = maxBalance;
+            MinBalance = minBalance;
         }
-    }
 
-    public void PrintTransactionHistory()
-    {
-        lock (historyLock) 
+        public void AddPayment(decimal amount)
         {
-            
-            Console.WriteLine("История платежей:");
-            foreach (decimal payment in History)
+            lock (historyLock)
             {
-                Console.WriteLine(payment);
+                if (amount > MaxBalance)
+                {
+                    throw new MaxBalanceExceededException($"Превышен максимальный баланс: {MaxBalance}");
+                }
+
+                if (amount < MinBalance)
+                {
+                    throw new MinBalanceExceededException($"Превышен минимальный баланс: {MinBalance}");
+                }
+
+                History.Add(amount);
+                Console.WriteLine($"Платеж на сумму {amount} добавлен.");
+            }
+        }
+
+        public void PrintTransactionHistory()
+        {
+            lock (historyLock)
+            {
+                Console.WriteLine("История платежей:");
+                foreach (decimal payment in History)
+                {
+                    Console.WriteLine(payment);
+                }
+            }
+        }
+
+        public void CancelTransactions()
+        {
+            lock (historyLock)
+            {
+                History.Clear();
+                Console.WriteLine("Транзакции успешно отменены.");
             }
         }
     }
-    
-}
+
+    public class MaxBalanceExceededException : Exception
+    {
+        public MaxBalanceExceededException(string message) : base(message)
+        {
+        }
+    }
+
+    public class MinBalanceExceededException : Exception
+    {
+        public MinBalanceExceededException(string message) : base(message)
+        {
+        }
+    }
--- HWLock/Program.cs
+++ HWLock/Program.cs
@@ -2,25 +2,59 @@
 
 using HWLock;
 
-PaymentProcessor processor = new PaymentProcessor();
+PaymentProcessor processor = new PaymentProcessor(500, 0);
 
+Thread addPaymentThread1 = new Thread(() =>
+{
+    try
+    {
+        processor.AddPayment(100);
+    }
+    catch (MaxBalanceExceededException e)
+    {
+        Console.WriteLine($"Ошибка: {e.Message}");
+    }
+    catch (MinBalanceExceededException e)
+    {
+        Console.WriteLine($"Ошибка: {e.Message}");
+    }
+});
 
-Thread addPaymentThread1 = new Thread(() => processor.AddPayment(100));
-Thread addPaymentThread2 = new Thread(() => processor.AddPayment(200));
-
+Thread addPaymentThread2 = new Thread(() =>
+{
+    try
+    {
+        processor.AddPayment(600);
+    }
+    catch (MaxBalanceExceededException e)
+    {
+        Console.WriteLine($"Ошибка: {e.Message}");
+    }
+    catch (MinBalanceExceededException e)
+    {
+        Console.WriteLine($"Ошибка: {e.Message}");
+    }
+});
 
 Thread printHistoryThread1 = new Thread(processor.PrintTransactionHistory);
 Thread printHistoryThread2 = new Thread(processor.PrintTransactionHistory);
-
 
 addPaymentThread1.Start();
 addPaymentThread2.Start();
 printHistoryThread1.Start();
 printHistoryThread2.Start();
 
-
 addPaymentThread1.Join();
 addPaymentThread2.Join();
 printHistoryThread1.Join();
 printHistoryThread2.Join();
+
+Console.WriteLine("Введите 'cancel', чтобы отменить все транзакции:");
+string input = Console.ReadLine();
+if (input == "cancel")
+{
+    processor.CancelTransactions();
+}
+
+Console.ReadLine();
 
